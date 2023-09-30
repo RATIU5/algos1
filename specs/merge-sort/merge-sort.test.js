ab07dd782b1b69b56b6176c9ca1fd909ada48c9f
RATIU5 <jcmemmott20@outlook.com>
2023-09-30 13:45:17 -0600

    add: implementation of merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,31 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length <= 1) {
+    return nums;
+  }
+
+  const middle = Math.floor(nums.length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle, nums.length);
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+  return merge(sortedLeft, sortedRight);
+};
+
+const merge = (left, right) => {
+  const array = [];
+  if (left[0] > right[0]) {
+    array.push(...right, ...left);
+  } else {
+    array.push(...left, ...right);
+  }
+  return array;
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function() {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
